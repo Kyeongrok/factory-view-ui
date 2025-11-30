2107cb4648287142700d2f13807767db5b40a601
kyeongrok.kim <kyeongrok.kim@e3view.com>
2025-11-30 21:32:16 +0900

    CloseButton추가

---
+++ FactoryView.Support/UI/Units/CloseButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FactoryView.Support.UI.Units;
+
+public class CloseButton : Button
+{
+    static CloseButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(CloseButton), 
+            new FrameworkPropertyMetadata(typeof(CloseButton)));
+    }
+    
+    protected override void OnClick()
+    {
+        base.OnClick();
+
+        var window = Window.GetWindow(this);
+        window?.Close();
+    }
+}
