709d52743eda4b5b9c02e09b0b6a618f79a3f9f6
kyeongrok.kim <kyeongrok.kim@e3view.com>
2025-12-03 23:18:07 +0900

    App.cs, Starter.cs 구조 적용

---
+++ FactoryView/App.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using FactoryView.Forms.UI.Views;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace FactoryView;
+
+internal class App : PrismApplication
+{
+    
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        // 서비스 등록 (필요 시 추가)
+        // containerRegistry.RegisterSingleton<IMyService, MyService>();
+
+        // 뷰 등록 (필요 시 추가)
+        // containerRegistry.RegisterForNavigation<MyView>();
+    }
+
+    protected override Window CreateShell()
+    {
+        return new FactoryViewWindow();
+    }
+}
---
+++ FactoryView/Starter.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace FactoryView;
+
+internal class Starter
+{
+    
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        _ = new App()
+            .Run();
+        
+    }
+}
